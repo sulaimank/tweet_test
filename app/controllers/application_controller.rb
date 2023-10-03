5a5fdfc70cc47912acbd515208247cbb4b386285
Sulaiman Karmali <skarmali@maestrohealth.com>
2023-10-03 10:47:56 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+# Author: Sulaiman Karmali
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
